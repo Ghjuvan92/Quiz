f24865193fc2e633376c4aa0049f60b9c93023e8
Ghjuvan92 <jcnaudin2@gmail.com>
2026-06-05 15:51:30 +0200

    New

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'ceb-v1';
+const FILES = [
+  '/Quiz/compte_est_bon.html',
+  '/Quiz/1.mp3',
+  '/Quiz/2.mp3',
+  '/Quiz/3.mp3',
+  '/Quiz/ElevenLabs_clackity_wheel_spin.mp3',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
